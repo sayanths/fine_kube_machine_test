abd42671f26fc286a659fd05c2377eaab87925ce
sayanth A <sayanthsayu619@gmail.com>
2022-09-15 19:23:20 +0530

    bottom nav index done

--- lib/bottom_navigation/view/bottom_navigation.dart
+++ lib/bottom_navigation/view/bottom_navigation.dart
@@ -9,54 +9,87 @@ class BottomNavigationBarSCreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    final  controler =context.read<BottomNavigationController>();
     return Scaffold(
       bottomNavigationBar: Consumer<BottomNavigationController>(
-        builder: (context, value, _) => Container(
-          height: size.height / 10,
-          color: const Color.fromARGB(255, 251, 251, 251),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 3,
-            padding: const EdgeInsets.all(2),
-            itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                onTap: () {
-                  value.changeIndex(index);
-                },
-                splashColor: const Color.fromARGB(0, 245, 242, 242),
-                highlightColor: Colors.red,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    AnimatedContainer(
-                      duration: const Duration(microseconds: 1500),
-                      curve: Curves.fastLinearToSlowEaseIn,
-                      margin: EdgeInsets.only(
-                        bottom:
-                            index == value.currentIndex ? 0 : size.width / 70,
-                        right: size.width / 12,
-                        left: size.width / 12,
+        builder: (context, value, _) => Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                height: size.height / 10,
+                color: const Color.fromARGB(255, 214, 11, 11),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () {
+                        value.changeIndex(index);
+                        value.widgetListBottomNav[index];
+                      },
+                      splashColor: const Color.fromARGB(0, 245, 242, 242),
+                      highlightColor: Colors.red,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          AnimatedContainer(
+                            duration: const Duration(microseconds: 1500),
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            margin: EdgeInsets.only(
+                              bottom: index == value.currentIndex
+                                  ? 0
+                                  : size.width / 80,
+                              right: size.width / 12,
+                              left: size.width / 12,
+                            ),
+                            width: size.width / 15,
+                            height: index == value.currentIndex
+                                ? size.height / 190
+                                : 0,
+                            decoration: const BoxDecoration(
+                              color: Colors.black,
+                            ),
+                          ),
+                          Icon(
+                            value.listOfIcons[index],
+                            size: size.width * .076,
+                            color: index == value.currentIndex
+                                ? Colors.black
+                                : grey,
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                        ],
                       ),
-                      width: size.width / 15,
-                      height:
-                          index == value.currentIndex ? size.height / 190 : 0,
-                      decoration: const BoxDecoration(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Icon(
-                      value.listOfIcons[index],
-                      size: size.width * .076,
-                      color: index == value.currentIndex ? Colors.black : grey,
-                    ),
-                  
-                  ],
+                    );
+                  },
                 ),
-              );
-            },
-          ),
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                width: 15,
+                height: 50,
+                child: Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  height: size.height / 10,
+                  decoration: BoxDecoration(
+                    color: yellow,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
+      body:controler.widgetListBottomNav[controler.currentIndex],
     );
   }
 }
--- lib/bottom_navigation/view_model/bottom_nav_controller.dart
+++ lib/bottom_navigation/view_model/bottom_nav_controller.dart
@@ -1,17 +1,25 @@
-import 'package:flutter/cupertino.dart';
+import 'package:fine_kube_machine_test/home_screen/view/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavigationController extends ChangeNotifier {
   var currentIndex = 0;
 
   changeIndex(newIndex) {
-    currentIndex= newIndex;
+    currentIndex = newIndex;
     notifyListeners();
   }
 
-    List<IconData> listOfIcons = [
+  List<IconData> listOfIcons = [
     Icons.home_rounded,
     Icons.favorite_rounded,
     Icons.settings_rounded,
   ];
+
+  List<Widget> widgetListBottomNav = [
+    const HomeScreen(),
+    const HomeScreen(),
+    const HomeScreen(),
+  ];
+
+  
 }
