9402af52e12da29aa608da8f6a177c6375458887
sayanth A <sayanthsayu619@gmail.com>
2022-09-15 15:49:53 +0530

    bottom nav 3 bottom created

--- lib/bottom_navigation/view/bottom_navigation.dart
+++ lib/bottom_navigation/view/bottom_navigation.dart
@@ -1,16 +1,61 @@
-import 'package:flutter/cupertino.dart';
+import 'package:fine_kube_machine_test/bottom_navigation/view_model/bottom_nav_controller.dart';
+import 'package:fine_kube_machine_test/core/colors/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
 
-class BottomNavigationBar extends StatelessWidget {
-  const BottomNavigationBar({Key? key}) : super(key: key);
+class BottomNavigationBarSCreen extends StatelessWidget {
+  const BottomNavigationBarSCreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      bottomNavigationBar: Container(
-        
+      bottomNavigationBar: Consumer<BottomNavigationController>(
+        builder: (context, value, _) => Container(
+          height: size.height / 10,
+          color: const Color.fromARGB(255, 251, 251, 251),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 3,
+            padding: const EdgeInsets.all(2),
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                onTap: () {
+                  value.changeIndex(index);
+                },
+                splashColor: const Color.fromARGB(0, 245, 242, 242),
+                highlightColor: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    AnimatedContainer(
+                      duration: const Duration(microseconds: 1500),
+                      curve: Curves.fastLinearToSlowEaseIn,
+                      margin: EdgeInsets.only(
+                        bottom:
+                            index == value.currentIndex ? 0 : size.width / 70,
+                        right: size.width / 12,
+                        left: size.width / 12,
+                      ),
+                      width: size.width / 15,
+                      height:
+                          index == value.currentIndex ? size.height / 190 : 0,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                      ),
+                    ),
+                    Icon(
+                      value.listOfIcons[index],
+                      size: size.width * .076,
+                      color: index == value.currentIndex ? Colors.black : grey,
+                    ),
+                  
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
--- lib/bottom_navigation/view_model/bottom_nav_controller.dart
+++ lib/bottom_navigation/view_model/bottom_nav_controller.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class BottomNavigationController extends ChangeNotifier{
+class BottomNavigationController extends ChangeNotifier {
+  var currentIndex = 0;
 
+  changeIndex(newIndex) {
+    currentIndex= newIndex;
+    notifyListeners();
+  }
+
+    List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.favorite_rounded,
+    Icons.settings_rounded,
+  ];
 }
