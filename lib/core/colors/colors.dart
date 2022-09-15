e6c6b76c2acba6975af8f3a1c0c8c3e90cd27292
sayanth A <sayanthsayu619@gmail.com>
2022-09-15 12:35:49 +0530

    for just demo color added

---
+++ lib/core/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const Color orange = Color.fromARGB(255, 236, 155, 6);
+const Color green = Color.fromARGB(255, 76, 175, 120);
+const Color blue = Color.fromARGB(255, 0, 44, 203);
+const Color grey = Color.fromARGB(255, 107, 107, 107);
+const Color yellow = Color.fromARGB(255, 234, 246, 22);
