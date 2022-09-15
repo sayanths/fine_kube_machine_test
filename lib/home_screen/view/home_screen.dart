847ed20836939cc8cb49b087eb5ab2db1fe9312c
sayanth A <sayanthsayu619@gmail.com>
2022-09-15 13:18:37 +0530

    sliver demo done

--- lib/home_screen/view/home_screen.dart
+++ lib/home_screen/view/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fine_kube_machine_test/core/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,9 +6,43 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: CustomScrollView(
-      slivers: [],
+      body: SizedBox(
+        height: size.height -
+            AppBar().preferredSize.height -
+            MediaQuery.of(context).padding.top,
+        width: size.width,
+        child:  CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              stretch: true,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                
+               // title: Text('app bar'),
+                background: Container(
+                  color: orange,
+                ),
+                stretchModes: <StretchMode>[
+                  StretchMode.blurBackground,
+                  
+                ],
+
+              ),
+              // onStretchTrigger: (() {
+              
+              // }), 
+            ),
+            SliverToBoxAdapter(
+          child: Container(
+            height: size.height,
+            width: 300,
+            color: blue,
+          )
+            )
+          ],
+        ),
       ),
     );
   }
