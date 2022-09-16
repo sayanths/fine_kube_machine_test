96ffbca904d67899a0dd2bc77a3868f99bdc18fb
sayanth A <sayanthsayu619@gmail.com>
2022-09-16 12:32:18 +0530

    upper container started

--- lib/home_screen/view/home_screen.dart
+++ lib/home_screen/view/home_screen.dart
@@ -1,5 +1,6 @@
-import 'package:fine_kube_machine_test/core/colors/colors.dart';
+
 import 'package:flutter/material.dart';
+import '../widgets/home_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,71 +10,17 @@ class HomeScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: ColoredBox(
-       color: yellow,
-        // child: CustomScrollView(
-        //   shrinkWrap: true,
-        //   physics: const BouncingScrollPhysics(),
-        //   slivers: [
-        //     SliverAppBar(
-        //       stretch: true,
-        //       expandedHeight: 260,
-        //       title: const Text("Home"),
-        //       leading: const CircleAvatar(),
-        //       actions: [
-        //         Image.asset(
-        //           "assets/notifications.png",
-        //           height: 5,
-        //         )
-        //       ],
-        //       centerTitle: true,
-        //       flexibleSpace: FlexibleSpaceBar(
-        //         background: Row(
-        //           mainAxisAlignment: MainAxisAlignment.center,
-        //           children: [
-        //             Container(
-        //               margin: const EdgeInsets.only(top: 90),
-        //               height: 180,
-        //               width: 140,
-        //               decoration: BoxDecoration(
-        //                   color: orange,
-        //                   borderRadius: BorderRadius.circular(20)),
-        //             ),
-        //           ],
-        //         ),
-        //         stretchModes: const <StretchMode>[
-        //           StretchMode.blurBackground,
-        //         ],
-        //       ),
-        //       // onStretchTrigger: (() {
-
-        //       // }),
-        //     ),
-        //     SliverToBoxAdapter(
-        //       child: Container(
-        //         height: size.height,
-        //         width: size.width,
-        //         decoration: const BoxDecoration(
-        //           color: Colors.white,
-        //           borderRadius: BorderRadius.only(
-        //             topLeft: Radius.circular(20),
-        //             topRight: Radius.circular(20),
-        //           ),
-        //         ),
-        //       ),
-        //     ),
-        //   ],
-        // ),
+        color: Colors.blue,
         child: CustomScrollView(
           slivers: <Widget>[
             SliverPersistentHeader(
-              pinned: false,
-              floating: true,
+              pinned: true,
+              floating: false,
               delegate: CustomSliverDelegate(
                 expandedHeight: 200,
               ),
             ),
             SliverFillRemaining(
-              
               child: Container(
                 height: size.height,
                 width: size.width,
@@ -93,76 +40,4 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class CustomSliverDelegate extends SliverPersistentHeaderDelegate {
-  final double expandedHeight;
-  final bool hideTitleWhenExpanded;
 
-  CustomSliverDelegate({
-    required this.expandedHeight,
-    this.hideTitleWhenExpanded = true,
-  });
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    final appBarSize = expandedHeight - shrinkOffset;
-   // final cardTopPosition = expandedHeight / 5 - shrinkOffset;
-    final proportion = 2 - (expandedHeight / appBarSize);
-    final percent = proportion < 0 || proportion > 1 ? 0.0 : proportion;
-    return SizedBox(
-      height: expandedHeight + expandedHeight / 2,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          SizedBox(
-           // height: appBarSize < kToolbarHeight ? kToolbarHeight : appBarSize,
-            child:Container(
-              color: yellow,
-              
-            ),
-          ),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            top:250,
-            bottom: -60.0,
-            child: Opacity(
-              opacity: percent,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30 * percent),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    CircleAvatar(
-                      radius: 40,
-                    ),
-                    CircleAvatar(
-                      radius: 40,
-                    ),
-                    CircleAvatar(
-                      radius: 40,
-                    ),
-                    CircleAvatar(
-                      radius: 40,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  double get maxExtent => expandedHeight + expandedHeight / 2;
-
-  @override
-  double get minExtent => kToolbarHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
-  }
-}
--- lib/home_screen/widgets/home_widget.dart
+++ lib/home_screen/widgets/home_widget.dart
@@ -1,46 +1,134 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import '../../core/colors/colors.dart';
 
-class SliverHeader extends StatelessWidget {
-  const SliverHeader({Key? key}) : super(key: key);
+class CustomSliverDelegate extends SliverPersistentHeaderDelegate {
+  final double expandedHeight;
+  final bool hideTitleWhenExpanded;
 
-  @override
-  Widget build(BuildContext context) {
-    return SliverPersistentHeader(
-      delegate: Delegate(),
-      pinned: true,
-      floating: false,
-    );
-  }
-}
+  CustomSliverDelegate({
+    required this.expandedHeight,
+    this.hideTitleWhenExpanded = true,
+  });
 
-class Delegate extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        CircleAvatar(),
-        CircleAvatar(),
-        CircleAvatar(),
-        CircleAvatar(),
-      ],
+    return SafeArea(
+      child: SizedBox(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  CircleAvatar(),
+                  Text(
+                    "Home",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Icon(Icons.notifications)
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 70, left: 15),
+                    height: 200,
+                    width: 160,
+                    decoration: BoxDecoration(
+                      color: orange,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: const [
+                                SizedBox(
+                                  height: 35,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 25),
+                                  child: Text(
+                                    "sdsd",
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: Image.asset(
+                                    "assets/bottom_left_arrow.png",
+                                    height: 60,
+                                    color: Colors.black.withOpacity(0.2),
+                                  )),
+                            ),
+                          ],
+                        ),
+                      
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              // height: appBarSize < kToolbarHeight ? kToolbarHeight : appBarSize,
+              child: ColoredBox(
+                color: Colors.blue,
+                child: Column(
+                  children: const [],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0.0,
+              right: 0.0,
+              top: 220,
+              bottom: -60.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  CircleAvatar(
+                    radius: 40,
+                  ),
+                  CircleAvatar(
+                    radius: 40,
+                  ),
+                  CircleAvatar(
+                    radius: 40,
+                  ),
+                  CircleAvatar(
+                    radius: 40,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   @override
-  // TODO: implement maxExtent
-  double get maxExtent => throw UnimplementedError();
+  double get maxExtent => expandedHeight + expandedHeight / 2;
 
   @override
-  // TODO: implement minExtent
-  double get minExtent => throw UnimplementedError();
+  double get minExtent => kToolbarHeight;
 
   @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    // TODO: implement shouldRebuild
-    throw UnimplementedError();
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
