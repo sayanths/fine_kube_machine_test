fe7013ecdaaff16163b17c8ec05aae76065280bf
sayanth A <sayanthsayu619@gmail.com>
2022-09-16 19:35:02 +0530

    color changed

--- lib/core/colors/colors.dart
+++ lib/core/colors/colors.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const Color orange = Color.fromARGB(255, 236, 155, 6);
-const Color green = Color.fromARGB(255, 76, 175, 120);
-const Color blue = Color.fromARGB(255, 0, 44, 203);
-const Color grey = Color.fromARGB(255, 107, 107, 107);
-const Color yellow = Color.fromARGB(255, 234, 246, 22);
+const Color orange =  Color(0XFFFB9275);
+const Color green = Color(0XFF82D4B3);
+const Color blue = Color(0XFF147587);
+const Color grey = Color(0XFF21262B);
+const Color yellow = Color(0XFFDDF247);
+const Color white = Color.fromARGB(255, 255, 255, 255);
+const Color black = Color.fromARGB(255, 0, 0, 0);
+const Color red = Color.fromARGB(255, 255, 0, 0);
